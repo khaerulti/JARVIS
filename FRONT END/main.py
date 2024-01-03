eac2c427a12799c742415448e680d9caa960f87f
khaerulti <khaerullti07055@gmail.com>
2024-01-04 03:49:14 +0800

    commit

--- FRONT END/main.py
+++ FRONT END/main.py
@@ -6,4 +6,4 @@ eel.init('FRONT END')
 
 os.system('start msedge.exe --app="http://localhost:8000/index.html"')
 
-eel.start('index.html', mode=None, host='localhost', blok=True)
+eel.start('index.html', mode=None, host='localhost', blok=True) 
